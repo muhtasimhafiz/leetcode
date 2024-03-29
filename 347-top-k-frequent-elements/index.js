8577b82d45ff927ca777a1eb26bc5fb57d98dd6f
DESKTOP-SVBVC1U\HP <muhtasimhafiz@gmail.com>
2024-03-29 21:42:36 +0600

    ...

--- 347-top-k-frequent-elements/index.js
+++ 347-top-k-frequent-elements/index.js
@@ -1,5 +1,20 @@
-function method(arg){
-    return '';
+function topKFrequent(nums, k) {
+    let hashmap = {};
+    for (let index = 0; index < nums.length; index++) {
+        const element = nums[index];
+        if (hashmap[element]) {
+            hashmap[element]++;
+        } else {
+            hashmap[element] = 1;
+        }
+    }
+
+    const sortedKeys = Object.keys(hashmap).sort((a, b) => {
+        return hashmap[b] - hashmap[a];
+    });
+
+    return sortedKeys.slice(0, k).map(key => parseInt(key));
+ 
 }
 
-module.exports = method;
+module.exports = topKFrequent;
--- 347-top-k-frequent-elements/test/test.js
+++ 347-top-k-frequent-elements/test/test.js
@@ -5,9 +5,20 @@ const method = require('../index.js');
 
 describe('Tests', () => {
     it('1', () => {
-        assert.equal(method(''),'');
+        assert.deepStrictEqual(method([1,1,1,2,2,3],2),[1,2]);
     });
     it('2', () => {
-        assert.equal(method(''),'');
+        assert.deepStrictEqual(method([1],1),[1]);
     });
+    it('3', () => {
+        assert.deepEqual(method([4,1,-1,2,-1,2,3]
+            ,2),[2,-1]);
+    });
+    it('4', () => {
+        assert.deepStrictEqual(method([],1),[]);
+    });
+    it('5', () => {
+        assert.deepStrictEqual(method([1,1,1,2,2,3],3),[1,2,3]);
+    });
+    
 });
